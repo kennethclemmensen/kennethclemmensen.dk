b7e4b3caa79b3d1bf21d1a7a317b049bc12a9383
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2017-11-27 17:31:07 +0100

    Renaming classes, methods and variables in the FDWC plugin

--- public/wp-content/plugins/fdwc/includes/class-fdwc-loader.php
+++ public/wp-content/plugins/fdwc/includes/class-fdwc-loader.php
@@ -5,18 +5,18 @@ namespace FDWC\Includes;
  * Class FDWC_Loader contains a method to load JavaScript files
  * @package FDWC\Includes
  */
-class FDWC_Loader {
+class FDWCLoader {
 
     /**
      * Load a JavaScript file
      */
-	public function load_scripts() {
-		add_action('wp_enqueue_scripts', function() {
-			$script = 'fdwc-js';
-			$script_file = 'assets/js/script.js';
-			$version = filemtime(__DIR__.'/../'.$script_file);
-			wp_enqueue_script($script, plugin_dir_url(__DIR__).$script_file, ['jquery'], $version, true);
-			wp_localize_script($script, 'fdwc_ajax', ['ajax_url' => admin_url('admin-ajax.php')]);
-		});
-	}
+    public function loadScripts() {
+        add_action('wp_enqueue_scripts', function() {
+            $script = 'fdwc-js';
+            $scriptFile = 'assets/js/script.js';
+            $version = filemtime(__DIR__.'/../'.$scriptFile);
+            wp_enqueue_script($script, plugin_dir_url(__DIR__).$scriptFile, ['jquery'], $version, true);
+            wp_localize_script($script, 'fdwc_ajax', ['ajax_url' => admin_url('admin-ajax.php')]);
+        });
+    }
 }
--- public/wp-content/plugins/fdwc/includes/class-fdwc.php
+++ public/wp-content/plugins/fdwc/includes/class-fdwc.php
@@ -9,288 +9,288 @@ use \WP_Query;
  */
 class FDWC {
 
-	private $_field_description;
-	private $_field_file;
-	private $_field_download_counter;
-	private $_field_file_type;
-	private $_tax_file_type;
+    private $fieldDescription;
+    private $fieldFile;
+    private $fieldDownloadCounter;
+    private $fieldFileType;
+    private $taxFileType;
 
-	const FDWC_FILE = 'fdwc_file';
+    const FDWC_FILE = 'fdwc_file';
 
     /**
      * FDWC constructor
      */
-	public function __construct() {
-		$prefix = 'fdwc_field_';
-		$this->_field_description = $prefix.'description';
-		$this->_field_file = $prefix.'file';
-		$this->_field_download_counter = $prefix.'download_counter';
-		$this->_field_file_type = $prefix.'file_type';
-		$prefix = 'fdwc_tax_';
-		$this->_tax_file_type = $prefix.'file_type';
-	}
+    public function __construct() {
+        $prefix = 'fdwc_field_';
+        $this->fieldDescription = $prefix.'description';
+        $this->fieldFile = $prefix.'file';
+        $this->fieldDownloadCounter = $prefix.'download_counter';
+        $this->fieldFileType = $prefix.'file_type';
+        $prefix = 'fdwc_tax_';
+        $this->taxFileType = $prefix.'file_type';
+    }
 
     /**
      * Activate the plugin
      *
-     * @param string $main_plugin_file the path to the main plugin file
+     * @param string $mainPluginFile the path to the main plugin file
      */
-	public function activate(string $main_plugin_file) {
-		register_activation_hook($main_plugin_file, function() {
-			if(!class_exists('RW_Meta_Box')) {
-				die('Meta Box is not activated');
-			}
-		});
-	}
+    public function activate(string $mainPluginFile) {
+        register_activation_hook($mainPluginFile, function() {
+            if(!class_exists('RW_Meta_Box')) {
+                die('Meta Box is not activated');
+            }
+        });
+    }
 
     /**
      * Execute the plugin
      */
-	public function execute() {
-		$this->load_depedencies();
-		$loader = new FDWC_Loader();
-		$loader->load_scripts();
-		$this->init();
-		$this->admin_menu();
-		$this->rwmb_meta_boxes();
-		$this->add_shortcode();
-		$this->upload_mimes();
-		$this->wp_ajax();
-	}
+    public function execute() {
+        $this->loadDepedencies();
+        $loader = new FDWCLoader();
+        $loader->loadScripts();
+        $this->init();
+        $this->adminMenu();
+        $this->addMetaBoxes();
+        $this->addShortcode();
+        $this->uploadMimes();
+        $this->wpAjax();
+    }
 
     /**
      * Load the dependencies files
      */
-	private function load_depedencies() {
-		require_once 'class-fdwc-loader.php';
-	}
+    private function loadDepedencies() {
+        require_once 'class-fdwc-loader.php';
+    }
 
     /**
      * Use the init action to register the fdwc file custom post type
      * and the file types taxonomy
      */
-	private function init() {
-		add_action('init', function() {
-			register_post_type(self::FDWC_FILE, [
-				'labels' => [
-					'name' => 'Files',
-					'singular_name' => 'File'
-				],
-				'public' => true,
-				'exclude_from_search' => true,
-				'has_archive' => true,
-				'supports' => ['title']
-			]);
-			register_taxonomy($this->_tax_file_type, self::FDWC_FILE, [
-				'labels' => [
-					'name' => 'File types',
-					'singular_name' => 'File type'
-				],
-				'show_admin_column' => true
-			]);
-			register_taxonomy_for_object_type($this->_tax_file_type, self::FDWC_FILE);
-		});
-	}
+    private function init() {
+        add_action('init', function() {
+            register_post_type(self::FDWC_FILE, [
+                'labels' => [
+                    'name' => 'Files',
+                    'singular_name' => 'File'
+                ],
+                'public' => true,
+                'exclude_from_search' => true,
+                'has_archive' => true,
+                'supports' => ['title']
+            ]);
+            register_taxonomy($this->taxFileType, self::FDWC_FILE, [
+                'labels' => [
+                    'name' => 'File types',
+                    'singular_name' => 'File type'
+                ],
+                'show_admin_column' => true
+            ]);
+            register_taxonomy_for_object_type($this->taxFileType, self::FDWC_FILE);
+        });
+    }
 
     /**
      * Use the admin_menu action to remove the File types meta box
      */
-	private function admin_menu() {
-		add_action('admin_menu', function() {
-			remove_meta_box('tagsdiv-'.$this->_tax_file_type, self::FDWC_FILE, 'normal');
-		});
-	}
+    private function adminMenu() {
+        add_action('admin_menu', function() {
+            remove_meta_box('tagsdiv-'.$this->taxFileType, self::FDWC_FILE, 'normal');
+        });
+    }
 
     /**
      * Use the rwmb_meta_boxes filter to add meta boxes to the fdwc file custom post type
      */
-	private function rwmb_meta_boxes() {
-		add_filter('rwmb_meta_boxes', function(array $meta_boxes) : array {
-			$meta_boxes[] = [
-				'id' => 'file_informations',
-				'title' => 'File informations',
-				'post_types' => [self::FDWC_FILE],
-				'fields' => [
-					[
-						'name' => 'Description',
-						'id' => $this->_field_description,
-						'type' => 'textarea'
-					],
-					[
-						'name' => 'File',
-						'id' => $this->_field_file,
-						'type' => 'file_advanced',
-						'max_file_uploads' => 1
-					],
-					[
-						'name' => 'Download counter',
-						'id' => $this->_field_download_counter,
-						'type' => 'number',
-						'std' => 0
-					],
-					[
-						'name' => 'File type',
-						'id' => $this->_field_file_type,
-						'type' => 'taxonomy',
-						'taxonomy' => $this->_tax_file_type,
-						'field_type' => 'select'
-					]
-				],
-				'validation' => [
-					'rules' => [
-						$this->_field_description => [
-							'required' => true
-						],
-						$this->_field_download_counter => [
-							'required' => true,
-							'min' => 0
-						],
-						$this->_field_file_type => [
-							'required' => true
-						]
-					]
-				]
-			];
-			return $meta_boxes;
-		});
-	}
+    private function addMetaBoxes() {
+        add_filter('rwmb_meta_boxes', function(array $metaBoxes) : array {
+            $metaBoxes[] = [
+                'id' => 'file_informations',
+                'title' => 'File informations',
+                'post_types' => [self::FDWC_FILE],
+                'fields' => [
+                    [
+                        'name' => 'Description',
+                        'id' => $this->fieldDescription,
+                        'type' => 'textarea'
+                    ],
+                    [
+                        'name' => 'File',
+                        'id' => $this->fieldFile,
+                        'type' => 'file_advanced',
+                        'max_file_uploads' => 1
+                    ],
+                    [
+                        'name' => 'Download counter',
+                        'id' => $this->fieldDownloadCounter,
+                        'type' => 'number',
+                        'std' => 0
+                    ],
+                    [
+                        'name' => 'File type',
+                        'id' => $this->fieldFileType,
+                        'type' => 'taxonomy',
+                        'taxonomy' => $this->taxFileType,
+                        'field_type' => 'select'
+                    ]
+                ],
+                'validation' => [
+                    'rules' => [
+                        $this->fieldDescription => [
+                            'required' => true
+                        ],
+                        $this->fieldDownloadCounter => [
+                            'required' => true,
+                            'min' => 0
+                        ],
+                        $this->fieldFileType => [
+                            'required' => true
+                        ]
+                    ]
+                ]
+            ];
+            return $metaBoxes;
+        });
+    }
 
     /**
      * Add the fdwc_files shortcode to show a list of files
      */
-	private function add_shortcode() {
-		add_shortcode('fdwc_files', function(array $atts) : string {
-			$html = '<div class="fdwc">';
-			$paged = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
-			$args = [
-				'post_type' => self::FDWC_FILE,
-				'posts_per_page' => 7,
-				'order' => 'ASC',
-				'tax_query' => [
-					[
-						'taxonomy' => $this->_tax_file_type,
-						'terms' => $atts['file_type_id']
-					]
-				],
-				'paged' => $paged
-			];
-			$wp_query = new WP_Query($args);
-			while($wp_query->have_posts()) {
-				$wp_query->the_post();
-				$html .= '<div class="fdwc__section">';
-				$html .= '<a href="'.$this->get_file_url().'" class="fdwc__link" rel="nofollow" data-post-id="'.get_the_ID().'" download>'.$this->get_file_name().'</a>';
-				$html .= '<p>'.$this->get_file_description().'</p>';
-				$html .= '<p>Antal downloads: '.$this->get_file_downloads().'</p>';
-				$html .= '</div>';
-			}
-			$big = 999999999; // need an unlikely integer
-			$replace = '%#%';
-			$html .= '<div class="fdwc__pagination">';
-			$html .= paginate_links([
-				'base' => str_replace($big, $replace, esc_url(get_pagenum_link($big))),
-				'format' => '?paged='.$replace,
-				'current' => max(1, $paged),
-				'total' => $wp_query->max_num_pages,
-				'prev_text' => 'Forrige',
-				'next_text' => 'Næste'
-			]);
-			$html .= '</div></div>';
-			return $html;
-		});
-	}
+    private function addShortcode() {
+        add_shortcode('fdwc_files', function(array $atts) : string {
+            $html = '<div class="fdwc">';
+            $paged = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
+            $args = [
+                'post_type' => self::FDWC_FILE,
+                'posts_per_page' => 7,
+                'order' => 'ASC',
+                'tax_query' => [
+                    [
+                        'taxonomy' => $this->taxFileType,
+                        'terms' => $atts['file_type_id']
+                    ]
+                ],
+                'paged' => $paged
+            ];
+            $wpQuery = new WP_Query($args);
+            while($wpQuery->have_posts()) {
+                $wpQuery->the_post();
+                $html .= '<div class="fdwc__section">';
+                $html .= '<a href="'.$this->getFileUrl().'" class="fdwc__link" rel="nofollow" data-post-id="'.get_the_ID().'" download>'.$this->getFileName().'</a>';
+                $html .= '<p>'.$this->getFileDescription().'</p>';
+                $html .= '<p>Antal downloads: '.$this->getFileDownloads().'</p>';
+                $html .= '</div>';
+            }
+            $big = 999999999; // need an unlikely integer
+            $replace = '%#%';
+            $html .= '<div class="fdwc__pagination">';
+            $html .= paginate_links([
+                'base' => str_replace($big, $replace, esc_url(get_pagenum_link($big))),
+                'format' => '?paged='.$replace,
+                'current' => max(1, $paged),
+                'total' => $wpQuery->max_num_pages,
+                'prev_text' => 'Forrige',
+                'next_text' => 'Næste'
+            ]);
+            $html .= '</div></div>';
+            return $html;
+        });
+    }
 
     /**
      * Use the upload_mimes filter to allow java files upload
      */
-	private function upload_mimes() {
-		$priority = 1;
-		add_filter('upload_mimes', function(array $mime_types) : array {
-			$mime_types['java'] = 'application/java';
-			return $mime_types;
-		}, $priority);
-	}
+    private function uploadMimes() {
+        $priority = 1;
+        add_filter('upload_mimes', function(array $mimeTypes) : array {
+            $mimeTypes['java'] = 'application/java';
+            return $mimeTypes;
+        }, $priority);
+    }
 
     /**
      * Use the wp_ajax wp_ajax_nopriv actions to update the counter
      */
-	private function wp_ajax() {
-		$action = 'fdwc_download';
-		add_action('wp_ajax_nopriv_'.$action, function() {
-			$this->update_counter();
-			wp_die();
-		});
-		add_action('wp_ajax_'.$action, function() {
-			$this->update_counter();
-			wp_die();
-		});
-	}
+    private function wpAjax() {
+        $action = 'fdwc_download';
+        add_action('wp_ajax_nopriv_'.$action, function() {
+            $this->updateCounter();
+            wp_die();
+        });
+        add_action('wp_ajax_'.$action, function() {
+            $this->updateCounter();
+            wp_die();
+        });
+    }
 
     /**
      * Update the counter for a file
      */
-	private function update_counter() {
-		$id = $_POST['post_id'];
-		$downloads = $this->get_file_downloads($id);
-		$downloads++;
-		update_post_meta($id, $this->_field_download_counter, $downloads);
-	}
+    private function updateCounter() {
+        $id = $_POST['post_id'];
+        $downloads = $this->getFileDownloads($id);
+        $downloads++;
+        update_post_meta($id, $this->fieldDownloadCounter, $downloads);
+    }
 
     /**
      * Get the file url
      *
-     * @param int|null $post_id the id of the post
+     * @param int|null $postID the id of the post
      * @param array $args an array of arguments
      * @return string the file url
      */
-	private function get_file_url(int $post_id = null, array $args = []) : string {
-		$file = rwmb_meta($this->_field_file, $args, $post_id);
-		$file = array_shift($file);
-		return $file['url'];
-	}
+    private function getFileUrl(int $postID = null, array $args = []) : string {
+        $file = rwmb_meta($this->fieldFile, $args, $postID);
+        $file = array_shift($file);
+        return $file['url'];
+    }
 
     /**
      * Get the file path
      *
-     * @param int|null $post_id the id of the post
+     * @param int|null $postID the id of the post
      * @param array $args an array of arguments
      * @return string the file path
      */
-	private function get_file_path(int $post_id = null, array $args = []) : string {
-		$file = rwmb_meta($this->_field_file, $args, $post_id);
-		$file = array_shift($file);
-		return $file['path'];
-	}
+    private function getFilePath(int $postID = null, array $args = []) : string {
+        $file = rwmb_meta($this->fieldFile, $args, $postID);
+        $file = array_shift($file);
+        return $file['path'];
+    }
 
     /**
      * Get the file name
      *
-     * @param int|null $post_id the id of the post
+     * @param int|null $postID the id of the post
      * @param array $args an array of arguments
      * @return string the file name
      */
-	private function get_file_name(int $post_id = null, array $args = []) : string {
-		return basename($this->get_file_path($post_id, $args));
-	}
+    private function getFileName(int $postID = null, array $args = []) : string {
+        return basename($this->getFilePath($postID, $args));
+    }
 
     /**
      * Get the file description
      *
-     * @param int|null $post_id the id of the post
+     * @param int|null $postID the id of the post
      * @param array $args an array of arguments
      * @return string the file description
      */
-	private function get_file_description(int $post_id = null, array $args = []) : string {
-		return rwmb_meta($this->_field_description, $args, $post_id);
-	}
+    private function getFileDescription(int $postID = null, array $args = []) : string {
+        return rwmb_meta($this->fieldDescription, $args, $postID);
+    }
 
     /**
      * Get the number of file downloads
      *
-     * @param int|null $post_id the id of the post
+     * @param int|null $postID the id of the post
      * @param array $args an array of arguments
      * @return int the number of file downloads
      */
-	private function get_file_downloads(int $post_id = null, array $args = []) : int {
-		return rwmb_meta($this->_field_download_counter, $args, $post_id);
-	}
+    private function getFileDownloads(int $postID = null, array $args = []) : int {
+        return rwmb_meta($this->fieldDownloadCounter, $args, $postID);
+    }
 }
