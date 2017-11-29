a77a60b57f39f094fcc7ba06b5fc4573f5fcac79
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2017-11-29 15:03:59 +0100

    Lightbox is updated to version 2.10

--- public/wp-content/plugins/kc-gallery/includes/KCGalleryLoader.php
+++ public/wp-content/plugins/kc-gallery/includes/KCGalleryLoader.php
@@ -13,8 +13,8 @@ class KCGalleryLoader {
     public function loadStylesAndScripts() {
         add_action('wp_enqueue_scripts', function() {
             $lightbox = 'lightbox';
-            wp_enqueue_style($lightbox, '//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.9.0/css/lightbox.css');
-            wp_enqueue_script($lightbox, '//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.9.0/js/lightbox.min.js', ['jquery'], false, true);
+            wp_enqueue_style($lightbox, '//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.10.0/css/lightbox.min.css');
+            wp_enqueue_script($lightbox, '//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.10.0/js/lightbox.min.js', ['jquery'], false, true);
         });
     }
 }
