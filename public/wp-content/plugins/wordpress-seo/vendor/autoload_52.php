a72999fed61c3717b054a3bc7b7f4812ba053721
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2017-12-11 13:09:34 +0100

    Yoast SEO is updated to version 5.9.2

--- public/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ public/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitb92e92465a0fd3dbcaf8feb5b9240372::getLoader();
+return ComposerAutoloaderInit8a8357a29a59592583f0693c224590f6::getLoader();
