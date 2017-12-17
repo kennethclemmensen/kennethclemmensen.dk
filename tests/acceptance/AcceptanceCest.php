8a7e7252dc17ece5a2b74cb9c9a1e8e05ae03fc6
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2017-12-17 20:19:07 +0100

    Create new acceptance tests

--- tests/acceptance/AcceptanceCest.php
+++ tests/acceptance/AcceptanceCest.php
@@ -1,8 +1,46 @@
 <?php
+/**
+ * The AcceptanceCest class contains acceptance tests for the website
+ */
 class AcceptanceCest {
 
-    public function frontpageWorks(AcceptanceTester $I) {
+    /**
+     * Test the front page
+     *
+     * @param AcceptanceTester $I the acceptance tester
+     */
+    public function testFrontPage(AcceptanceTester $I) {
+        $I->wantToTest('the front page');
         $I->amOnPage('/');
         $I->see('Velkommen');
+    }
+
+    /**
+     * Test a gallery page
+     *
+     * @param AcceptanceTester $I the acceptance tester
+     */
+    public function testGalleryPage(AcceptanceTester $I) {
+        $I->wantToTest('a gallery page');
+        $I->amOnPage('/');
+        $I->click('Billeder');
+        $I->amOnPage('/billeder');
+        $I->click('Fyn');
+        $I->amOnPage('/billeder/fyn');
+        $I->see('Fyn');
+    }
+
+    /**
+     * Test the search page
+     *
+     * @param AcceptanceTester $I the acceptance tester
+     */
+    public function testSearchPage(AcceptanceTester $I) {
+        $I->wantToTest('the search page');
+        $I->amOnPage('/');
+        $I->click('Søg');
+        $I->amOnPage('/soeg');
+        $I->fillField('search', 'Film');
+        $I->see('Søgeresultater');
     }
 }
