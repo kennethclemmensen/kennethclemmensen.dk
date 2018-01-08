23a7d61c41042af0317445ccbfce8a05f3d10d7d
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2018-01-08 19:23:19 +0100

    Added script.min.js to the FDWC plugin

--- public/wp-content/plugins/fdwc/includes/FDWCLoader.php
+++ public/wp-content/plugins/fdwc/includes/FDWCLoader.php
@@ -13,7 +13,7 @@ class FDWCLoader {
     public function loadScripts() {
         add_action('wp_enqueue_scripts', function() {
             $script = 'fdwc-js';
-            $scriptFile = 'assets/js/script.js';
+            $scriptFile = 'assets/js/script.min.js';
             $version = filemtime(__DIR__.'/../'.$scriptFile);
             wp_enqueue_script($script, plugin_dir_url(__DIR__).$scriptFile, ['jquery'], $version, true);
             wp_localize_script($script, 'fdwc_ajax', ['ajax_url' => admin_url('admin-ajax.php')]);
