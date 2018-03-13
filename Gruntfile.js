977564d0f02b43a6f927eb43f347218fdb3d8641
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2018-03-13 20:09:36 +0100

    Updated Grunt to use the assemble-less plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify-es');
-    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('assemble-less');
     grunt.loadNpmTasks('grunt-contrib-sassjs');
     grunt.loadNpmTasks('grunt-browser-sync');
 
