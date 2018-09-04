1f928cab114475a018d48ad768f72bfece320f2e
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2018-09-04 18:45:27 +0200

    Updated the code in the theme

--- public/wp-content/themes/kennethclemmensen/functions.php
+++ public/wp-content/themes/kennethclemmensen/functions.php
@@ -100,7 +100,7 @@ add_filter('style_loader_src', function(string $src) : string {
 });
 
 add_filter('script_loader_tag', function(string $tag) : string {
-    return str_replace(" type='text/javascript'", '', $tag);
+    return str_replace(" type='text/javascript'", ' defer', $tag);
 });
 
 define('DISALLOW_FILE_EDIT', true);
