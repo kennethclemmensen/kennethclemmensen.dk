0e188139a7e0ed5c6d3fba531f0bee7c27c133e1
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2018-09-11 21:45:28 +0200

    Updated Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
-let gulp = require('gulp');
-let browserSync = require('browser-sync').create();
+let p = require('./package.json');
+let gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    notify = require('gulp-notify');
 
 gulp.task('browser-sync', function() {
     browserSync.init({
-        proxy: "kennethclemmensen.test"
+        proxy: p.name + '.test'
     });
 });
 
 gulp.task('default', function() {
-    gulp.start('browser-sync');
+    gulp.start('browser-sync', 'watch');
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(p.jsFolderPath + '*.js')
+        .pipe(notify({message: 'Scripts task complete', onLast: true}));
+});
+
+gulp.task('styles', function() {
+    return gulp.src(p.lessFolderPath + '**/*.less')
+        .pipe(notify({message: 'Styles task complete', onLast: true}));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./' + p.jsFolderPath + '*.js', ['scripts']);
+    gulp.watch('./' + p.lessFolderPath + '**/*.less', ['styles']);
 });
