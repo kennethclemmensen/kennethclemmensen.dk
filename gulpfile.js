92ad01dda5539b51ce04acc149b960fd1568d6d3
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2018-09-12 20:12:31 +0200

    Configuring of the scripts, styles and watch Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 let p = require('./package.json');
-let gulp = require('gulp'),
-    browserSync = require('browser-sync'),
-    notify = require('gulp-notify');
+let gulp = require('gulp');
+let browserSync = require('browser-sync');
+let sass = require('gulp-sass');
+let cssnano = require('gulp-cssnano');
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglifyes');
 
 gulp.task('browser-sync', function() {
     browserSync.init({
@@ -24,15 +27,25 @@ gulp.task('default', function() {
 
 gulp.task('scripts', function() {
     return gulp.src(p.jsFolderPath + '*.js')
-        .pipe(notify({message: 'Scripts task complete', onLast: true}));
+        .pipe(concat('script.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(p.jsFolderPath + 'minified/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('styles', function() {
-    return gulp.src(p.lessFolderPath + '**/*.less')
-        .pipe(notify({message: 'Styles task complete', onLast: true}));
+    return gulp.src(p.sassFolderPath + 'style.scss')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(gulp.dest(p.cssFolderPath))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./' + p.jsFolderPath + '*.js', ['scripts']);
-    gulp.watch('./' + p.lessFolderPath + '**/*.less', ['styles']);
+    gulp.watch(p.jsFolderPath + '*.js', ['scripts']);
+    gulp.watch(p.sassFolderPath + '**/*.scss', ['styles']);
 });
