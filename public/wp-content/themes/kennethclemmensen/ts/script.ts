a3a69af5262988a17eba1e2fb52593faa6fb4a3b
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2018-10-18 17:12:22 +0200

    Updated TypeScript files

---
+++ public/wp-content/themes/kennethclemmensen/ts/script.ts
@@ -0,0 +1,75 @@
+declare let jQuery: any;
+declare let lightbox: any;
+
+jQuery.noConflict();
+(function($) {
+    $(document).ready(function() {
+        $('.header__nav-trigger').on('click', function(event) {
+            event.preventDefault();
+            $(this).toggleClass('header__nav-trigger--active');
+            $('.mobile-nav').toggleClass('mobile-nav--active');
+            $('html, body').toggleClass('show-mobile-nav');
+        });
+        $('.mobile-nav__arrow').on('click', function(event) {
+            event.preventDefault();
+            $(this).toggleClass('mobile-nav__arrow--rotated');
+            $(this).parent().parent().find('.sub-menu').toggle();
+        });
+
+        /**
+         * The Slider class contains methods to handle the functionality of the slider
+         */
+        class Slider {
+
+            delay: number;
+            duration: number;
+            $sliderImages: any;
+
+            /**
+             * Slider constructor
+             *
+             * @param delay the delay of a slide
+             * @param duration the duration of a slide
+             */
+            constructor(delay, duration) {
+                this.delay = delay;
+                this.duration = duration;
+                this.$sliderImages = $('.slider__image');
+                this.show();
+            }
+
+            /**
+             * Get a random number
+             *
+             * @returns {number} a random number
+             */
+            getRandomNumber() {
+                return Math.floor(Math.random() * this.$sliderImages.length);
+            }
+
+            /**
+             * Show the slider
+             */
+            show() {
+                let randomNumber = this.getRandomNumber();
+                this.$sliderImages.eq(randomNumber).show();
+                let self = this;
+                setInterval(function() {
+                    self.$sliderImages.eq(randomNumber).fadeOut(self.duration, function() {
+                        self.$sliderImages.eq(randomNumber).hide(); //prevent display block on more than one image
+                        randomNumber = self.getRandomNumber();
+                        self.$sliderImages.eq(randomNumber).fadeIn(self.duration);
+                    });
+                }, this.delay);
+            }
+        }
+
+        let $slider = $('#slider');
+        new Slider($slider.data('delay'), $slider.data('duration'));
+
+        let $body = $('body');
+        lightbox.option({
+            'albumLabel': $body.data('image-text') + ' %1 ' + $body.data('of-text') + ' %2'
+        });
+    });
+})(jQuery);
---
+++ public/wp-content/themes/kennethclemmensen/ts/search-app.ts
@@ -0,0 +1,88 @@
+declare let Vue: any;
+
+const app = new Vue({
+    el: '#search-app',
+    data: {
+        searchString: '',
+        results: []
+    },
+    watch: {
+        searchString: function () {
+            this.search();
+        }
+    },
+    methods: {
+        search: function () {
+            if (this.searchString === '') {
+                this.results = [];
+                return;
+            }
+            let self = this;
+            let statusCodeOk = 200;
+            let request = new XMLHttpRequest();
+            request.open('get', '/wp-json/kcapi/v1/pages/' + this.searchString, true);
+            request.onload = function () {
+                self.results = (request.status === statusCodeOk) ? JSON.parse(request.responseText) : [];
+            };
+            request.onerror = function () {
+                self.results = [];
+            };
+            request.send();
+        }
+    },
+    components: {
+        'search-results': {
+            data: function () {
+                return {
+                    currentPage: 0,
+                    offset: 0,
+                    perPage: 5
+                }
+            },
+            methods: {
+                previousPage: function () {
+                    this.currentPage--;
+                    this.offset = this.currentPage * this.perPage;
+                },
+                nextPage: function () {
+                    this.currentPage++;
+                    this.offset = this.currentPage * this.perPage;
+                }
+            },
+            props: {
+                results: {
+                    required: true,
+                    type: Array
+                },
+                previousText: {
+                    required: true,
+                    type: String
+                },
+                nextText: {
+                    required: true,
+                    type: String
+                }
+            },
+            watch: {
+                results: function () {
+                    this.currentPage = 0;
+                    this.offset = 0;
+                }
+            },
+            template: `
+                <div>
+                    <div v-for="(result, index) in results" :key="result.id">
+                        <span v-if="index >= offset && index < offset + perPage">
+                            <a :href="result.link">{{ result.title }}</a>
+                            <p>{{ result.excerpt }}</p>
+                        </span>
+                    </div>
+                    <div>
+                        <a href="#" @click.prevent="previousPage" v-if="offset > 0">{{ previousText }}</a>
+                        <a href="#" @click.prevent="nextPage" v-if="offset < (results.length - perPage)">{{ nextText }}</a>
+                    </div>
+                </div>
+            `
+        }
+    }
+});
