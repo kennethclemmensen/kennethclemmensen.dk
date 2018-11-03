f95379a1c0aa357c61fb746901ade9598f0a9d9d
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2018-11-03 08:52:31 +0100

    Font Awesome is updated to version 5.5.0

--- public/wp-content/themes/kennethclemmensen/functions.php
+++ public/wp-content/themes/kennethclemmensen/functions.php
@@ -10,8 +10,8 @@ require_once 'includes/TranslationStrings.php';
  */
 add_action('wp_enqueue_scripts', function() : void {
     $fontAwesome = 'font-awesome';
-    $cdnFile = 'https://use.fontawesome.com/releases/v5.4.2/css/all.css';
-    $localFile = '/css/fontawesome-5.4.2.min.css';
+    $cdnFile = 'https://use.fontawesome.com/releases/v5.5.0/css/all.css';
+    $localFile = '/css/fontawesome-5.5.0.min.css';
     $version = filemtime(get_template_directory().$localFile);
     ThemeHelper::addStyleWithLocalFallback($fontAwesome, $cdnFile, get_template_directory_uri().$localFile, [], $version);
 
