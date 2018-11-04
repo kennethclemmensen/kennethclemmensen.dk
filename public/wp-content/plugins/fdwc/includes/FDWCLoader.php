76cb68743a6f7535af46c78646a1099e4272445e
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2018-11-04 14:46:31 +0100

    Updated the FDWC plugin

--- public/wp-content/plugins/fdwc/includes/FDWCLoader.php
+++ public/wp-content/plugins/fdwc/includes/FDWCLoader.php
@@ -13,7 +13,7 @@ class FDWCLoader {
     public function loadScripts() : void {
         add_action('wp_enqueue_scripts', function() : void {
             $script = 'fdwc-js';
-            $scriptFile = 'assets/js/script.min.js';
+            $scriptFile = 'public/js/script.min.js';
             $version = filemtime(__DIR__.'/../'.$scriptFile);
             wp_enqueue_script($script, plugin_dir_url(__DIR__).$scriptFile, ['jquery'], $version, true);
             wp_localize_script($script, 'fdwc_ajax', ['ajax_url' => admin_url('admin-ajax.php')]);
