51aa3387e9c5b19c3283b59d7bfeeb1ab19abe31
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2019-02-18 16:43:10 +0100

    Vue is updated to version 2.6.6

--- public/wp-content/themes/kennethclemmensen/functions.php
+++ public/wp-content/themes/kennethclemmensen/functions.php
@@ -32,8 +32,8 @@ add_action('wp_enqueue_scripts', function() : void {
     ThemeHelper::addScriptWithLocalFallback($jquery, $cdnFile, get_template_directory_uri().$localFile, $version);
 
     $vue = 'vue';
-    $cdnFile = 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.4/vue.min.js';
-    $localFile = '/js/libraries/vue-2.6.4.min.js';
+    $cdnFile = 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.6/vue.min.js';
+    $localFile = '/js/libraries/vue-2.6.6.min.js';
     $version = filemtime(get_template_directory().$localFile);
     ThemeHelper::addScriptWithLocalFallback($vue, $cdnFile, get_template_directory_uri().$localFile, $version);
 
