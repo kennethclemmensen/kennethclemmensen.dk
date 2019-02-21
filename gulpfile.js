ea78fb5024e56b3a00c9b9141765265ab344a969
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2019-02-21 20:56:47 +0100

    Updated Gulp to version 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
-let p = require('./package.json');
-let gulp = require('gulp');
-let browserSync = require('browser-sync');
-let sass = require('gulp-sass');
-let less = require('gulp-less');
-let cssnano = require('gulp-cssnano');
-let concat = require('gulp-concat');
-let uglify = require('gulp-uglifyes');
-let imagemin = require('gulp-imagemin');
-let ts = require('gulp-typescript');
-let tsConfig = ts.createProject('tsconfig.json');
+const { dest, series, src, watch } = require('gulp');
+const browserSync = require('browser-sync');
+const concat = require('gulp-concat');
+const cssnano = require('gulp-cssnano');
+const imagemin = require('gulp-imagemin');
+const less = require('gulp-less');
+const p = require('./package.json');
+const sass = require('gulp-sass');
+const ts = require('gulp-typescript');
+const tsConfig = ts.createProject('tsconfig.json');
+const uglify = require('gulp-uglifyes');
 
-gulp.task('browser-sync', function() {
+exports.imagemin = function() {
+    return src(p.uploadsFolderPath + '**')
+        .pipe(imagemin())
+        .pipe(dest(p.uploadsFolderPath));
+};
+
+exports.default = series(function() {
     browserSync.init({
         debugInfo: true,
         files: [
@@ -25,67 +31,50 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('default', function() {
-    gulp.start('browser-sync', 'watch');
-});
-
-gulp.task('imagemin', function() {
-    return gulp.src(p.uploadsFolderPath + '**')
-        .pipe(imagemin())
-        .pipe(gulp.dest(p.uploadsFolderPath));
-});
-
-gulp.task('javascript', function() {
-    return gulp.src(p.jsFolderPath + '*.js')
+watch(p.jsFolderPath + '*.js', function() {
+    return src(p.jsFolderPath + '*.js')
         .pipe(concat('script.min.js'))
         .pipe(uglify())
         .on('error', function(error) {
             console.log(error.toString());
             this.emit('end');
         })
-        .pipe(gulp.dest(p.jsFolderPath + 'minified/'))
+        .pipe(dest(p.jsFolderPath + 'minified/'))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
-gulp.task('less', function() {
-    return gulp.src(p.lessFolderPath + 'style.less')
+watch(p.lessFolderPath + '**/*.less', function() {
+    return src(p.lessFolderPath + 'style.less')
         .pipe(less())
         .on('error', function(error) {
             console.log(error.toString());
             this.emit('end');
         })
         .pipe(cssnano())
-        .pipe(gulp.dest(p.cssFolderPath))
+        .pipe(dest(p.cssFolderPath))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
-gulp.task('sass', function() {
-    return gulp.src(p.sassFolderPath + 'style.scss')
+watch(p.sassFolderPath + '**/*.scss', function() {
+    return src(p.sassFolderPath + 'style.scss')
         .pipe(sass())
         .on('error', function(error) {
             console.log(error.toString());
             this.emit('end');
         })
         .pipe(cssnano())
-        .pipe(gulp.dest(p.cssFolderPath))
+        .pipe(dest(p.cssFolderPath))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
-gulp.task('typescript', function() {
+watch(p.tsFolderPath + '*.ts', function() {
     return tsConfig.src()
         .pipe(tsConfig())
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('watch', function() {
-    gulp.watch(p.jsFolderPath + '*.js', ['javascript']);
-    gulp.watch(p.lessFolderPath + '**/*.less', ['less']);
-    gulp.watch(p.sassFolderPath + '**/*.scss', ['sass']);
-    gulp.watch(p.tsFolderPath + '*.ts', ['typescript']);
+        .pipe(dest(tsConfig.options.outDir));
 });
