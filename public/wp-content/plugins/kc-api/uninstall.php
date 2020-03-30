4b6c59350764fa6248b3d206688b978f101a24ec
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2020-03-30 16:21:29 +0200

    Removed the FDWC, KC API, KC Gallery & KC Slider plugins

--- public/wp-content/plugins/kc-api/uninstall.php
+++
@@ -1,3 +0,0 @@
-<?php
-namespace KCAPI;
-if(!defined('WP_UNINSTALL_PLUGIN')) die();
--- public/wp-content/plugins/kc-slider/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace KCSlider;
-if(!defined('WP_UNINSTALL_PLUGIN')) die();
-require_once 'includes/KCSlider.php';
-require_once 'includes/KCSliderSettings.php';
-$kcSliderSettings = Includes\KCSliderSettings::getInstance();
-$posts = get_posts(['post_type' => Includes\KCSlider::SLIDES, 'posts_per_page' => -1]);
-foreach($posts as $post) wp_delete_post($post->ID, true);
-delete_option($kcSliderSettings->getOptionName());
