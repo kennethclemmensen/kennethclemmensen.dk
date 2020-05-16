8b5b3777dcb8bdd3a9f330c5cf6d4be53db8c558
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2020-05-16 09:31:10 +0200

    Added wpbrowser

--- tests/_support/WpunitTester.php
+++ tests/_support/WpunitTester.php
@@ -16,9 +16,9 @@
  *
  * @SuppressWarnings(PHPMD)
 */
-class FunctionalTester extends \Codeception\Actor
+class WpunitTester extends \Codeception\Actor
 {
-    use _generated\FunctionalTesterActions;
+    use _generated\WpunitTesterActions;
 
    /**
     * Define custom actions here
