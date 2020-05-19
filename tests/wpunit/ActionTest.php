116bb539e552dd119c6d391fc71bd7d4e98088de
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2020-05-19 15:43:46 +0200

    Added wpunit tests

---
+++ tests/wpunit/ActionTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace KC\Core;
+
+use \Codeception\TestCase\WPTestCase;
+
+/**
+ * The ActionTest class contains methods to test the Action class
+ */
+class ActionTest extends WPTestCase {
+    
+    /**
+     * Test the getSavePostAction method
+     */
+    public function testGetSavePostAction() : void {
+        $expected = 'save_post_gallery';
+        $this->assertEquals($expected, Action::getSavePostAction(PostType::GALLERY));
+    }
+
+    /**
+     * Test the getManagePostsCustomColumn method
+     */
+    public function testGetManagePostsCustomColumn() : void {
+        $expected = 'manage_photo_posts_custom_column';
+        $this->assertEquals($expected, Action::getManagePostsCustomColumn(PostType::IMAGE));
+    }
+}
