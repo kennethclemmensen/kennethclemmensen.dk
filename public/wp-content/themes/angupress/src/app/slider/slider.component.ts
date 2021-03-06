9279b43bb61734d39f75f14a1160812169a17344
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2021-03-06 15:14:37 +0100

    Updated the page and slider components

--- public/wp-content/themes/angupress/src/app/slider/slider.component.ts
+++ public/wp-content/themes/angupress/src/app/slider/slider.component.ts
@@ -7,9 +7,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
-  constructor() { }
+  //@ts-ignore
+  private slides: string[];
+  private sliderImage: HTMLElement | null;
+  private currentRandomNumber: number;
 
-  ngOnInit(): void {
+  public constructor() {
+    this.slides = [
+      'assets/images/image_1.jpg',
+      'assets/images/image_2.jpg'
+    ];
+    this.sliderImage = null;
+    this.currentRandomNumber = -1;
   }
 
+  public ngOnInit(): void {
+    this.sliderImage = document.getElementById('slider-image');
+    this.showSlides();
+  }
+
+  /**
+   * Show the slides
+   */
+  private showSlides(): void {
+    let delay: number = 500;
+    let duration: number = 8000;
+    let randomNumber: number = this.getRandomNumber();
+    let backgroundImageUrl: string | null = this.slides[randomNumber];
+    if (!backgroundImageUrl) return;
+    this.setBackgroundImage(backgroundImageUrl);
+    let startKeyframes: Keyframe[] = [{ opacity: 1 }, { opacity: 0 }];
+    let endKeyframes: Keyframe[] = [{ opacity: 0 }, { opacity: 1 }];
+    setInterval((): void => {
+      if (this.sliderImage) {
+        this.sliderImage.animate(startKeyframes, {
+          duration: delay
+        }).onfinish = (): void => {
+          randomNumber = this.getRandomNumber();
+          backgroundImageUrl = this.slides[randomNumber];
+          if (backgroundImageUrl) this.setBackgroundImage(backgroundImageUrl);
+          this.sliderImage?.animate(endKeyframes, { duration: delay });
+        };
+      }
+    }, duration);
+  }
+
+  /**
+   * Get a random number between 0 and the number of slides minus 1
+   * 
+   * @returns a random number
+   */
+  private getRandomNumber(): number {
+    let randomNumber: number = Math.floor(Math.random() * this.slides.length);
+    if (this.currentRandomNumber === randomNumber) return this.getRandomNumber();
+    this.currentRandomNumber = randomNumber;
+    return this.currentRandomNumber;
+  }
+
+  /**
+   * Set a background image on the slider image
+   * 
+   * @param backgroundImageUrl the background image url
+   */
+  private setBackgroundImage(backgroundImageUrl: string): void {
+    if (this.sliderImage) this.sliderImage.style.backgroundImage = 'url("' + backgroundImageUrl + '")';
+  }
 }
