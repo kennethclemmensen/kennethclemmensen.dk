cdac1b3161c16a370bfb4ef6c310ec650066dc07
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2021-03-11 16:20:21 +0100

    Changed Requires at least to 5.7

--- public/wp-content/plugins/kc/kc.php
+++ public/wp-content/plugins/kc/kc.php
@@ -5,7 +5,7 @@ Description: The plugin for the site
 Version: 1.0
 Author: Kenneth Clemmensen
 Author URI: https://kennethclemmensen.dk
-Requires at least: 5.6
+Requires at least: 5.7
 Requires PHP: 8.0
 Text Domain: kc
 Domain Path: /languages
