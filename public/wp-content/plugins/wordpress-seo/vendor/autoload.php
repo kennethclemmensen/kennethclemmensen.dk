be669b0aca33ce975321ee978786d2ebc63eb736
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2021-03-16 16:02:36 +0100

    Yoast SEO is updated to version 16.0

--- public/wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ public/wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitecd889bb68d63334e737994c9ea9ddcd::getLoader();
+return ComposerAutoloaderInitf641d2216e404dcc52ad01d6900a8594::getLoader();
