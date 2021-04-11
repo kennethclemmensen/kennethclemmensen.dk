84339c2559d9883b8e7ae6d4a89460a811f343ba
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2021-04-11 05:59:58 +0200

    Redirection is updated to version 5.1

--- public/wp-content/plugins/redirection/redirection-version.php
+++ public/wp-content/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '5.0.1' );
-define( 'REDIRECTION_BUILD', '3aaca2e8b96d0af2cc585f8590d6feb3' );
+define( 'REDIRECTION_VERSION', '5.1' );
+define( 'REDIRECTION_BUILD', 'a303333ae5185be5433467d110cae802' );
 define( 'REDIRECTION_MIN_WP', '4.6' );
