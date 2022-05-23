8b6b56eb0ec605e7ab7b8f17c258dc2f998f0313
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2022-05-23 17:00:18 +0200

    Added the terser-webpack-plugin package

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,20 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
     mode: 'production',
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+                terserOptions: {
+                    format: {
+                        comments: false
+                    }
+                }
+            })
+        ]
+    },
     performance: {
         maxAssetSize: 400000
     }
--- webpack.js.js
+++ webpack.js.js
@@ -14,7 +14,6 @@ module.exports = merge(common, {
         new mergeIntoSingleFilePlugin({
             files: [{
                 src: [
-                    'node_modules/rxjs/dist/bundles/rxjs.umd.min.js',
                     'node_modules/jquery/dist/jquery.min.js',
                     'node_modules/lightbox2/dist/js/lightbox.min.js',
                     'node_modules/lodash/lodash.min.js',
