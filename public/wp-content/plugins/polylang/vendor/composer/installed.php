4afdbe4ee4bce688a0ad34c8e0fe0375e0725714
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2023-10-14 05:25:00 +0200

    Polylang is updated to version 3.5

--- public/wp-content/plugins/polylang/vendor/composer/installed.php
+++ public/wp-content/plugins/polylang/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'wpsyntex/polylang',
-        'pretty_version' => '3.2.x-dev',
-        'version' => '3.2.9999999.9999999-dev',
-        'reference' => '9555976eb2dc703519184632f83fa6114d3a9ce3',
+        'pretty_version' => '3.5.x-dev',
+        'version' => '3.5.9999999.9999999-dev',
+        'reference' => '7a7377f97884b5df865b0400b1fe854a6741fe8f',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'wpsyntex/polylang' => array(
-            'pretty_version' => '3.2.x-dev',
-            'version' => '3.2.9999999.9999999-dev',
-            'reference' => '9555976eb2dc703519184632f83fa6114d3a9ce3',
+            'pretty_version' => '3.5.x-dev',
+            'version' => '3.5.9999999.9999999-dev',
+            'reference' => '7a7377f97884b5df865b0400b1fe854a6741fe8f',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
