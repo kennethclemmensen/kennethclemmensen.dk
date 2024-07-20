91ae557b3b83b864dab9e0514a8e70ab9fa23fad
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2024-07-20 05:40:15 +0200

    Show linter errors as warnings

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import onlyWarn from 'eslint-plugin-only-warn';
 
 export default tseslint.config(
 	eslint.configs.recommended,
   	...tseslint.configs.strict,
 	{
+		plugins: {
+			onlyWarn
+		},
 		rules: {
 			'no-undef': 'off',
 			'no-unused-vars': 'off',
