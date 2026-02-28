fe397a8b415c8c90d681c0be3476ff4d3f651d80
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2026-02-28 05:33:55 +0100

    Redirection is updated to version 5.7.3

--- public/wp-content/plugins/redirection/redirection-version.php
+++ public/wp-content/plugins/redirection/redirection-version.php
@@ -1,6 +1,6 @@
 <?php
 
 // This is a dummy file to help with cached sites
-define( 'REDIRECTION_VERSION', '5.6.1' );
-define( 'REDIRECTION_BUILD', '951ae5be919314a6585a78d8091577b5' );
-define( 'REDIRECTION_MIN_WP', '6.4' );
+define( 'REDIRECTION_VERSION', '5.7.3' );
+define( 'REDIRECTION_BUILD', '4226b5957b97db33450c269681d4a29e' );
+define( 'REDIRECTION_MIN_WP', '6.5' );
